111b069e624f76b024540f1453b709699d6e5f5c
Rozina <rozina@Aamirs-MBP.broadband>
2022-04-07 23:56:21 +0100

    created files for wordle

---
+++ src/components/Board.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import { boardDefault } from '../Words'
+
+function Board() {
+    const [board, setBoard] = useState(boardDefault);
+  return (
+    <div className="board">
+    <div className="row"></div>
+    <div className="row"></div>
+    <div className="row"></div>
+    <div className="row"></div>
+    <div className="row"></div>
+    <div className="row"></div>
+    </div>
+  )
+}
+
+export default Board
