50195ce47e06496ccef64e821d11b9c1dacf5cfe
Rozina <rozina@Aamirs-MBP.broadband>
2022-04-08 01:10:13 +0100

    added map function

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,8 +1,18 @@
 import React from 'react'
 
 function Keyboard() {
+  const keys1 = ["Q,", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
+  const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
+  const keys3= ["Z", "X", "C", "V", "B", "N", "M"];
+
   return (
-    <div>Keyboard</div>
+    <div className = "keyboard"> 
+    <div className = "line1">{keys1.map((Key) => 
+    return <div> {key} </div>
+  })} </div>
+    <div className = "line2"> </div>
+    <div className = "line3"> </div>
+    </div>
   )
 }
 
